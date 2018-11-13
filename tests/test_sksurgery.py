767de8d4089ae08222728fa5c31427ed42e1b04c
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2018-11-13 13:00:00 +0000

    Remove Unittest style tests

--- tests/test_sksurgery.py
+++ tests/test_sksurgery.py
@@ -4,17 +4,6 @@
 
 from sksurgery.ui.sksurgery_demo import run_demo
 
-# Unittest style test case
-from unittest import TestCase
-
-class Testsksurgery(TestCase):
-    def test_using_unittest_sksurgery(self):
-        return_value = run_demo(True, "Hello world")
-        self.assertTrue(return_value)
-
-
-# Pytest style
-
 def test_using_pytest_sksurgery():
     assert run_demo(True, "Hello World") == True
 
