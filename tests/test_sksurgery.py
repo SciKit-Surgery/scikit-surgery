475447c2872e1ee978fc4fae85e06d7aae119090
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2018-11-13 16:53:17 +0000

    Testing readthedocs webhook

--- tests/test_sksurgery.py
+++ tests/test_sksurgery.py
@@ -3,7 +3,7 @@
 """scikit-surgery tests"""
 
 from sksurgery.ui.sksurgery_demo import run_demo
+from sksurgery.ui import sksurgery_command_line
 
 def test_using_pytest_sksurgery():
     assert run_demo(True, "Hello World") == True
-
